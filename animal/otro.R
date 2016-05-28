3b6f1e36919f99197a422c1b0c3459faf3d39f2a
German Martinez <germaaan@gmail.com>
2016-05-28 17:47:52 +0200

    #2. Prueba con xgboost

---
+++ animal/otro.R
@@ -0,0 +1,212 @@
+library(rpart)
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+library(stringr)
+library(xgboost)
+library(randomForest)
+
+###############################################################################
+# This section follows Megan Risdal's 'Quick and Dirty Random Forest' Script  #
+# See her excellent script for more details                                   #
+###############################################################################
+
+train <- read.csv('/home/germaaan/proyectos/titanic-kaggle/animal/train.csv', stringsAsFactors=FALSE)
+test <- read.csv('/home/germaaan/proyectos/titanic-kaggle/animal/test.csv', stringsAsFactors=FALSE)
+
+train %>% head()
+test %>% head()
+
+colnames(train)[1] <- 'ID'
+test$ID <- as.character(test$ID)
+
+full <- bind_rows(train, test)
+
+# Get the time value:
+full$TimeValue <- sapply(full$AgeuponOutcome,  
+                         function(x) strsplit(x, split = ' ')[[1]][1])
+
+# Now get the unit of time:
+full$UnitofTime <- sapply(full$AgeuponOutcome,  
+                          function(x) strsplit(x, split = ' ')[[1]][2])
+
+# Fortunately any "s" marks the plural, so we can just pull them all out
+full$UnitofTime <- gsub('s', '', full$UnitofTime)
+
+full$TimeValue  <- as.numeric(full$TimeValue)
+full$UnitofTime <- as.factor(full$UnitofTime)
+
+# Make a multiplier vector
+multiplier <- ifelse(full$UnitofTime == 'day', 1,
+                     ifelse(full$UnitofTime == 'week', 7,
+                            ifelse(full$UnitofTime == 'month', 30, # Close enough
+                                   ifelse(full$UnitofTime == 'year', 365, NA))))
+
+# Apply our multiplier
+full$AgeinDays <- full$TimeValue * multiplier
+
+# Replace blank names with "Nameless"
+full$Name <- ifelse(nchar(full$Name)==0, 'Nameless', full$Name)
+
+# Make a name v. no name variable
+full$HasName[full$Name == 'Nameless'] <- 0
+full$HasName[full$Name != 'Nameless'] <- 1
+
+# Replace blank sex with most common
+full$SexuponOutcome <- ifelse(nchar(full$SexuponOutcome)==0, 
+                              'Spayed Female', full$SexuponOutcome)
+
+# Extract time variables from date (uses the "lubridate" package)
+full$Hour    <- hour(full$DateTime)
+full$Weekday <- wday(full$DateTime)
+full$Month   <- month(full$DateTime)
+full$Year    <- year(full$DateTime)
+
+# Time of day may also be useful
+full$TimeofDay <- ifelse(full$Hour > 5 & full$Hour < 11, 'morning',
+                         ifelse(full$Hour > 10 & full$Hour < 16, 'midday',
+                                ifelse(full$Hour > 15 & full$Hour < 20, 'lateday', 'night')))
+
+# Put factor levels into the order we want
+full$TimeofDay <- factor(full$TimeofDay, 
+                         levels = c('morning', 'midday',
+                                    'lateday', 'night'))
+
+summary(full$AgeinDays)
+
+# Use "grepl" to look for "Mix"
+full$IsMix <- ifelse(grepl('Mix', full$Breed), 1, 0)
+
+# Split on "/" and remove " Mix" to simplify Breed
+full$SimpleBreed <- sapply(full$Breed, 
+                           function(x) gsub(' Mix', '', 
+                                            strsplit(x, split = '/')[[1]][1]))
+
+# Use strsplit to grab the first color
+full$SimpleColor <- sapply(full$Color, 
+                           function(x) strsplit(x, split = '/| ')[[1]][1])
+
+levels(factor(full$SimpleColor))
+
+# Use "grepl" to look for "Intact"
+full$Intact <- ifelse(grepl('Intact', full$SexuponOutcome), 1,
+                      ifelse(grepl('Unknown', full$SexuponOutcome), 'Unknown', 0))
+
+# Use "grepl" to look for sex
+full$Sex <- ifelse(grepl('Male', full$SexuponOutcome), 'Male',
+                   ifelse(grepl('Unknown', full$Sex), 'Unknown', 'Female'))
+
+# Use rpart to predict the missing age values
+age_fit <- rpart(AgeinDays ~ AnimalType + Sex + Intact + SimpleBreed + HasName, 
+                 data = full[!is.na(full$AgeinDays), ], 
+                 method = 'anova')
+
+# Impute predicted age values where missing using "predict"
+full$AgeinDays[is.na(full$AgeinDays)] <- predict(age_fit, full[is.na(full$AgeinDays), ])
+
+# All gone? Yes.
+sum(is.na(full$AgeinDays))
+
+# Use the age variable to make a puppy/kitten variable
+full$Lifestage[full$AgeinDays < 365] <- 'baby'
+full$Lifestage[full$AgeinDays >= 365] <- 'adult'
+
+full$Lifestage <- factor(full$Lifestage)
+
+factorVars <- c('Name','OutcomeType','OutcomeSubtype','AnimalType',
+                'SexuponOutcome','AgeuponOutcome','SimpleBreed','SimpleColor',
+                'HasName','IsMix','Intact','Sex','TimeofDay','Lifestage')
+
+full[factorVars] <- lapply(full[factorVars], function(x) as.factor(x))
+
+# Split up train and test data
+train <- full[1:26729, ]
+test  <- full[26730:nrow(full), ]
+
+# prepare for xgboost model - is data correct type?
+class(train$OutcomeType)
+
+# No. Need to change to numeric
+y_train <- as.numeric(as.factor(train$OutcomeType)) - 1
+
+# keep track of the labels
+labels_train <- data.frame(train$OutcomeType, y_train)
+
+# xgboost-specific design matrices
+xgb_train <- xgb.DMatrix(model.matrix(~AnimalType+AgeinDays+Intact+
+                                        HasName+Hour+Weekday+TimeofDay+
+                                        SimpleColor+IsMix+Sex+Month, data=train),
+                         label=y_train, missing=NA)
+xgb_test <- xgb.DMatrix(model.matrix(~AnimalType+AgeinDays+Intact+
+                                       HasName+Hour+Weekday+TimeofDay+
+                                       SimpleColor+IsMix+Sex+Month, data=test), missing=NA)
+
+# build model
+xgb_model <- xgboost(xgb_train, y_train, nrounds=45, objective='multi:softprob',
+                     num_class=5, eval_metric='mlogloss',
+                     early.stop.round=TRUE)
+
+# make predictions
+predictions <- predict(xgb_model, xgb_test)
+
+# reshape predictions
+xgb_preds <- data.frame(t(matrix(predictions, nrow=5, ncol=length(predictions)/5)))
+
+# name columns
+colnames(xgb_preds) <- c('Adoption', 'Died', 'Euthanasia', 'Return_to_owner', 'Transfer')
+
+# attach ID column
+xgb_preds['ID'] <- test['ID']
+
+# quick peek - looks good
+head(xgb_preds)
+
+# build model - i like verbose output ;-)
+rf <- randomForest(OutcomeType~AnimalType+AgeinDays+Intact+
+                     HasName+Hour+Weekday+TimeofDay+
+                     SimpleColor+IsMix+Sex+Month, data=train,
+                   importance=FALSE, do.trace=1, ntree=550)
+
+# make predictions
+rf_preds <- data.frame(predict(rf, test, type='vote'))
+
+# take a look
+head(rf_preds)
+
+library(gridExtra)
+g1 <- ggplot() + geom_point(aes(x=rf_preds$Adoption, y=xgb_preds$Adoption),
+                            color='steelblue', alpha=0.3) +
+  labs(x='Random Forest', y='XGBoost') +
+  ggtitle('Adoption Predictions')
+g2 <- ggplot() + geom_point(aes(x=rf_preds$Died, y=xgb_preds$Died),
+                            color='steelblue', alpha=0.3) +
+  labs(x='Random Forest', y='XGBoost') +
+  ggtitle('Died Predictions')
+g3 <- ggplot() + geom_point(aes(x=rf_preds$Euthanasia, y=xgb_preds$Euthanasia),
+                            color='steelblue', alpha=0.3) + 
+  labs(x='Random Forest', y='XGBoost') +
+  ggtitle('Euthanasia Predictions')
+g4 <- ggplot() + geom_point(aes(x=rf_preds$Return_to_owner, y=xgb_preds$Return_to_owner),
+                            color='steelblue', alpha=0.3) + 
+  labs(x='Random Forest', y='XGBoost') + 
+  ggtitle('Return To Owner Predictions')
+g5 <- ggplot() + geom_point(aes(x=rf_preds$Transfer, y=xgb_preds$Transfer),
+                            color='steelblue', alpha=0.3) + 
+  labs(x='Random Forest', y='XGBoost') +
+  ggtitle('Transfer Predictions')
+
+grid.arrange(g1, g2, g3, g4, g5, ncol=3, nrow=2)
+
+ave_pred <- xgb_preds
+
+# drop ID column for averaging
+ave_pred <- ave_pred[,1:5]
+ave_pred %>% head()
+
+# average predictions
+ave_pred <- 0.5*(ave_pred+rf_preds)
+
+ave_pred %>% head()
+
+# write the submission file
+write.csv(ave_pred, 'rf_xgb_avg.csv', row.names=FALSE)
---
+++ animal/prueba.R
@@ -0,0 +1,135 @@
+# Animal Outcomes dataset on Kaggle #
+# title: 'Animal outcomes by decision trees'
+# author: 'Apurva Naik (@ Yvonne)'
+# date: '4 April 2016'
+
+install.packages("ggthemes")
+install.packages("lubridate")
+
+library(ggplot2) # visualization
+library(ggthemes) # visualizationbo
+library(dplyr) # data manipulation
+library(lubridate) # dates
+library(rpart) # rpart for imputation
+library(randomForest) # classification algorithm
+
+#load the train set
+outcome.train <- read.csv('/home/germaaan/proyectos/titanic-kaggle/animal/train.csv', header = T, stringsAsFactors = F)
+
+#load the test set
+outcome.test <- read.csv('/home/germaaan/proyectos/titanic-kaggle/animal/test.csv', header = T, stringsAsFactors = F)
+
+# Convert the ID column in test set the char and change column name of AnimalID in train set to make them consistent with each other
+outcome.test$ID = as.character(outcome.test$ID)
+names(outcome.train)[1] <- 'ID'
+
+# Combine the two data sets
+outcomes = bind_rows(outcome.train, outcome.test)
+
+# Clean the data set
+
+outcome.train$Name = ifelse(nchar(outcome.train$Name)==0, 'Nameless', outcome.train$Name)
+
+# create another column which indicates if an animal has a name or not, represented by either 0 or 1 and add to the data frame
+hasName = ifelse(outcomes$Name == 'Nameless', 0, 1)
+outcomes = data.frame(hasName, outcomes)
+
+# extract time variables from date/time data using lubridate (credit: MeganRisdal)
+outcomes$Hour = hour(outcomes$DateTime)
+outcomes$Weekday = wday(outcomes$DateTime)
+outcomes$Month = month(outcomes$DateTime)
+outcomes$Year = year(outcomes$DateTime)
+
+# I'm not sure how useful time of day is, from the prediction point of view, but we can see
+outcomes$TimeofDay = ifelse(outcomes$Hour > 5 & outcomes$Hour < 11, 'morning', ifelse(outcomes$Hour > 11 & outcomes$Hour < 16, 'midday', ifelse (outcomes$Hour > 16 & outcomes$Hour < 20, 'evening', 'night')))
+
+# Convert time of the day in factor levels
+outcomes$TimeofDay = factor(outcomes$TimeofDay, levels  = c('morning', 'midday', 'evening', 'night'))
+
+# AgeinOutcome column also has age data in different units. Convert all age in days (credit: MeganRisdal)
+
+# get the time value
+outcomes$TimeValue = sapply(outcomes$AgeuponOutcome, function(x) strsplit(x, split = ' ')[[1]][1])
+
+# get unit of time
+outcomes$UnitofTime = sapply(outcomes$AgeuponOutcome, function(x) strsplit(x, split = ' ')[[1]][2])
+
+# Remove all plural forms of units from the UnitofTime column. (eg. 'years' - 's' = 'year')
+outcomes$UnitofTime = gsub('s', '', outcomes$UnitofTime)
+
+# Convert UnitOfTime in factor and TimeValue in numeric
+outcomes$UnitofTime = as.factor(outcomes$UnitofTime)
+outcomes$TimeValue = as.numeric(outcomes$TimeValue)
+
+# calculate the age of the animal in days by converting TimeValue in days using the appropriate multiplier based on UnitofTime
+multiplier = ifelse(outcomes$UnitofTime == 'day', 1, ifelse(outcomes$UnitofTime == 'week', 7, ifelse(outcomes$UnitofTime == 'month', 30, ifelse(outcomes$UnitofTime == 'year', 365, NA))))
+outcomes$AgeinDays = multiplier * outcomes$TimeValue
+
+# Replace blank sex with most common after finding the most common one
+barplot(table(outcomes$SexuponOutcome))
+outcomes$SexuponOutcome = ifelse(nchar(outcomes$SexuponOutcome) == 0, "Neutered Male", outcomes$SexuponOutcome)
+
+# The factors (Breed, Color) have more many unique levels
+
+# Create table showing frequency of each levels occurrence
+Breed.table = data.frame(table(outcomes$Breed))
+Color.table = data.frame(table(outcomes$Color))
+# Order the table in descending order of frequency
+Breed.table = Breed.table[order(-Breed.table$Freq),]
+Color.table = Color.table[order(-Color.table$Freq),]
+
+# As we want to use randomForest we choose the top 31 levels. So, we leave the top 31 levels unchanged|Get values of the top 31 occuring levels
+noChange1 <- Breed.table$Var1[1:31]
+noChange2 <- Color.table$Var1[1:31]
+# we use 'Other' as factor to avoid overlap w/ other levels (ie if '32' was actually one of the levels). ifelse() checks to see if the factor level is in the list of the top 51 levels. If present it uses it as is, if not it changes it to 'Other'
+outcomes$newFactorBreed <- (ifelse(outcomes$Breed %in% noChange1, outcomes$Breed, "Other"))
+outcomes$newFactorColor <- (ifelse(outcomes$Color %in% noChange2, outcomes$Color, "Other"))
+
+# Impute missing age values by replacing the NAs with th mean age
+outcomes$AgeinDays = ifelse(is.na(outcomes$AgeinDays), mean(outcomes$AgeinDays, na.rm = T), outcomes$AgeinDays)
+
+# Replace all blank fields in the OutcomeSubType colum with 'Other'
+outcomes$OutcomeSubtype = ifelse(nchar(outcomes$OutcomeSubtype) == 0, 'Other', outcomes$OutcomeSubtype)
+
+
+# Factorize the data for Classification
+factorize = c('OutcomeType', 'OutcomeSubtype', 'AnimalType', 'SexuponOutcome', 'AgeuponOutcome', 'newFactorBreed', 'newFactorColor', 'hasName')
+outcomes[factorize] <- lapply(outcomes[factorize], function(x) as.factor(x))
+
+# convert Hour into numeric
+Hour = as.numeric(outcomes$Hour)
+
+print("Starting Random Forest Classification")
+
+# Start with Random Forest classification: split data set back into its original test and train set
+
+outcome.train = outcomes[1:26729, ]
+outcome.test = outcomes[26730:nrow(outcomes), ]
+
+#randomForest  
+
+clases <- OutcomeType ~ 
+  +AnimalType+SexuponOutcome+Hour+Weekday+AgeinDays+newFactorBreed+newFactorColor
+
+modelo <- train(clases, data=outcome.train, method="parRF", trControl=trainControl(method="cv", number=5))
+modelo
+
+prediccion <- predict(modelo, outcome.test)
+
+id <- seq(1, 11456)
+submission <- data.frame(ID=id, prediccion)
+submission$prediccion <- as.character(submission$prediccion)
+submission$Adoption <- 0
+submission$Died <- 0
+submission$Euthanasia <- 0
+submission$Return_to_owner <- 0
+submission$Transfer <- 0
+
+submission$Adoption[submission$prediccion == "Adoption"] <- 1
+submission$Died[submission$prediccion == "Died"] <- 1
+submission$Euthanasia[submission$prediccion == "Euthanasia"] <- 1
+submission$Return_to_owner[submission$prediccion == "Return_to_owner"] <- 1
+submission$Transfer[submission$prediccion == "Transfer"] <- 1
+submission$prediccion <- NULL
+
+write.csv(submission, file = "submission.rf.csv", row.names = F)
