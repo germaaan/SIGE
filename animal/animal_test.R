39fad8a6b3000a6c8c19f76682b9ead00b8d9028
German Martinez <germaaan@gmail.com>
2016-05-31 03:56:49 +0200

    Añade variables de tiempo

--- animal/animal_test.R
+++ animal/animal_test.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(rpart)
 library(caret)
 library(xgboost)
+library(lubridate)
 
 
 # Inicializamos semilla fija para que no aparezca un resulta nuevo cada vez
@@ -20,6 +21,26 @@ todo <- bind_rows(train, test)
 
 # Clasificamos perros por tener o no tener nombre
 todo$Name <- ifelse(todo$Name=="", 0, 1)
+
+# Añadimos aspectos referentes a la fecha
+todo$Hora <- hour(todo$DateTime)
+todo$Horario <- ifelse(todo$Hora >= 6 & todo$Hora < 14, 'Mañana', 
+                       ifelse(todo$Hora >= 14 & todo$Hora < 22, 'Tarde', 'Noche'))
+
+todo$Dia <- weekdays(as.Date(todo$DateTime))
+todo$Laborable[todo$Dia == 'lunes'] <- 1
+todo$Laborable[todo$Dia == 'martes'] <- 1
+todo$Laborable[todo$Dia == 'miércoles'] <- 1
+todo$Laborable[todo$Dia == 'jueves'] <- 1
+todo$Laborable[todo$Dia == 'viernes'] <- 1
+todo$Laborable[todo$Dia == 'sábado'] <- 0
+todo$Laborable[todo$Dia == 'domingo'] <- 0
+
+todo$Mes <- month(todo$DateTime)
+todo$Estacion <- ifelse(todo$Mes == 4 | todo$Mes == 5 | todo$Mes == 6, 'Primavera', 
+                        ifelse(todo$Mes == 7 | todo$Mes == 8 | todo$Mes == 9, 'Verano',
+                               ifelse(todo$Mes == 10 | todo$Mes == 11 | todo$Mes == 12, 'Otoño',
+                                      "Invierno")))
 
 # Clasificamos por sexo
 todo$SexuponOutcome[todo$SexuponOutcome == ''] <- "Unknown"
@@ -58,6 +79,9 @@ todo$Edad[is.na(todo$Edad)] <- predict(modeloEdad, todo[is.na(todo$Edad), ])
 
 todo$OutcomeType <- as.numeric(factor(todo$OutcomeType)) - 1
 todo$Name <- as.numeric(factor(todo$Name)) - 1
+todo$Horario <- as.numeric(factor(todo$Horario)) - 1
+todo$Laborable <- as.numeric(factor(todo$Laborable)) - 1
+todo$Estacion <- as.numeric(factor(todo$Estacion)) - 1
 todo$AnimalType <- as.numeric(factor(todo$AnimalType)) - 1
 todo$Sexo <- as.numeric(factor(todo$Sexo)) - 1
 todo$Estado <- as.numeric(factor(todo$Estado)) - 1
@@ -69,7 +93,7 @@ todo$Color <- as.numeric(factor(todo$Color)) - 1
 # clustering
 
 # Volvemos a separar los datos en sus respectivos conjuntos de entrenamiento y validación
-train <- todo[1:nrow(train), c(4, 2, 6, 11, 12, 17, 13, 14, 10)]
+train <- todo[1:nrow(train), c(4, 2, 12, 14, 16, 6, 17, 18, 23, 19, 20, 10)]
 
 # Crear particiones para entrenamiento y validacion
 in.train <- createDataPartition(y=train$OutcomeType, p=0.80, list=FALSE)
@@ -97,28 +121,23 @@ xg.param <- list("objective" = "multi:softprob",
                  'nthread' = 3)
 
 # Cross-Validation
-xgb.fit.cv <- xgb.cv(param = xg.param, data = train.x, label = train.y, 
-                     nfold = 5, nrounds = 1000)
-
-cv.min <- min(xgb.fit.cv$test.mlogloss.mean)
-cv.min.rounds <- which(xgb.fit.cv$test.mlogloss.mean == min(xgb.fit.cv$test.mlogloss.mean)) 
-
-cv.rounds <- round(mean(which(xgb.fit.cv$test.mlogloss.mean == min(xgb.fit.cv$test.mlogloss.mean))))  
+ajuste.cv <- xgb.cv(param = xg.param, data = train.x, label = train.y, nfold = 5, nrounds = 1000)
+num_rondas <- round(mean(which(ajuste.cv$test.mlogloss.mean == min(ajuste.cv$test.mlogloss.mean))))  
 
 # Ajustar modelo en el conjunto de validacion
-xgb.fit <- xgboost(param = xg.param, data = train.x[-in.train, ], 
-                   label = train.y[-in.train], nrounds = cv.rounds)
+validacion <- xgboost(param = xg.param, data = train.x[-in.train, ], 
+                   label = train.y[-in.train], nrounds = num_rondas)
 
 # Ajustar modelo en el conjunto de entrenamiento completo
-xgb.fit <- xgboost(param = xg.param, data = train.x, label = train.y, nrounds = cv.rounds)
+ajuste <- xgboost(param = xg.param, data = train.x, label = train.y, nrounds = num_rondas)
 
 # Prediccion
-test <- todo[(nrow(train)+1):nrow(todo), c(2, 6, 11, 12, 17, 13, 14, 10)]
+test <- todo[(nrow(train)+1):nrow(todo), c(2, 12, 14, 16, 6, 17, 18, 23, 19, 20, 10)]
 test <- as.matrix(test)
 test <- matrix(data = as.numeric(test), nrow = nrow(test), ncol = ncol(test))
 
-xgb.pred <- predict(xgb.fit, test)
-xgb.pred <- t(matrix(xgb.pred, nrow = 5, ncol = length(xgb.pred)/5))
-xgb.pred <- data.frame(1:nrow(xgb.pred), xgb.pred)
-names(xgb.pred) <- c("ID", "Adoption", "Died", "Euthanasia", "Return_to_owner", "Transfer")
-write.csv(xgb.pred, file='animal_solution.csv', quote=FALSE, row.names=FALSE)
+prediccion <- predict(ajuste, test)
+prediccion <- t(matrix(prediccion, nrow = 5, ncol = length(prediccion)/5))
+prediccion <- data.frame(1:nrow(prediccion), prediccion)
+names(prediccion) <- c("ID", "Adoption", "Died", "Euthanasia", "Return_to_owner", "Transfer")
+write.csv(prediccion, file='animal_solution.csv', quote=FALSE, row.names=FALSE)
